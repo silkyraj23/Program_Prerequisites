df38f218ef09b46b6f53401cc1982542435a6812
Shamiksha <silkyraj23@gmail.com>
2025-01-08 20:43:11 +0530

    Ques-9 Done

---
+++ Average.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Average{
+public static void main(String args[]){
+Scanner sc= new Scanner(System.in);
+int num_1=sc.nextInt();
+int num_2=sc.nextInt();
+int num_3=sc.nextInt();
+int average=(num_1+num_2+num_3)/3;
+System.out.println("Average of three number is " + average);
+}
+}
