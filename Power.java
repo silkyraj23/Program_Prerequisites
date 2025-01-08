4321cf86647faabb85729aae477870bef9c1cc20
Shamiksha <silkyraj23@gmail.com>
2025-01-08 20:37:18 +0530

    Ques-8 Done

---
+++ Power.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Power{
+public static void main(String args[]){
+Scanner sc= new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int power=(int) Math.pow(a,b);
+System.out.println("power of the given num is " + power);
+}
+}
+
