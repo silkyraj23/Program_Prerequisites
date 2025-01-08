2bd27c7f2db667d8b1e11fc166955579547d3c6c
Shamiksha <silkyraj23@gmail.com>
2025-01-08 20:25:23 +0530

    Ques-7 Done

---
+++ Perimeter.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Perimeter{
+public static void main(String args[]){
+Scanner sc= new Scanner(System.in);
+int length=sc.nextInt();
+int breadth=sc.nextInt();
+int perimeter = 2*(length+breadth);
+System.out.println("perimeter of a rectangle = " + perimeter);
+}
+}
