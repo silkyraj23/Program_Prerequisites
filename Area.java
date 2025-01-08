d821ddbc65f65f32c1b9eaffb9c4dc64908d890c
Shamiksha <silkyraj23@gmail.com>
2025-01-08 19:11:05 +0530

    Ques-4 Done

---
+++ Area.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Area{
+public static void main(String args[]){
+Scanner sc= new Scanner(System.in);
+int radius=sc.nextInt();
+double area=3.14*(radius*radius);
+System.out.println("Area of circle is "+ area);
+}
+}
