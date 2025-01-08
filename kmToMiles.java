311bac2f7f481abff249260a31d918ac148cccf0
Shamiksha <silkyraj23@gmail.com>
2025-01-08 20:54:56 +0530

    ques-10 Done

---
+++ kmToMiles.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class kmToMiles{
+public static void main(String args[]){
+Scanner sc= new Scanner(System.in);
+int km=sc.nextInt();
+double miles=km* 0.621371;
+System.out.println("Distance in miles " + miles );
+}
+}
